e5dc73c90b1ac930712ae25a2f490ef6dfc3a122
Patrick Brito <122879831+PatrickBritoK@users.noreply.github.com>
2023-01-31 17:26:37 -0300

    Add files via upload

---
+++ PrimeiroProjeto.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.primeiroprojeto;
+
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+      String texto = "Pulsati";
+      //tipo nomeVariavel = Algo
+      carro objeto1 = new carro();
+      objeto1.cilindrada = 1000;
+      objeto1.imprimeCarro();
+      
+      carro objeto2 = new carro();
+      objeto2.cilindrada = 1600;
+      objeto2.imprimeCarro();
+    }
+}
---
+++ carro.java
@@ -0,0 +1,12 @@
+package com.mycompany.primeiroprojeto;
+
+public class carro {
+
+    int cilindrada;
+    String cor;
+    String montadora;
+
+    public void imprimeCarro() {
+        System.out.println("O carro tem " + cilindrada + "Cilindradas")
+    }
+}
